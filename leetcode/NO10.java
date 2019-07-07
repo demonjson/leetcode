a5388744755862d05a31e3f293560e61d1ca02d7
demonjson <37144429+demonjson@users.noreply.github.com>
2019-07-07 21:01:36 +0800

    Update NO10.java

--- leetcode/NO10.java
+++ leetcode/NO10.java
@@ -6,7 +6,7 @@ public class NO10 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		String str = "ab";
 		String pattern = ".*c";
 		System.out.println(new NO10().isMatch(str, pattern));
