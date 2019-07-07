0ef687a7840199dda748a688c1db3844d7b4b929
demonjson <2250971434@qq.com>
2019-07-07 21:07:24 +0800

    修改

--- leetcode/NO10.java
+++ leetcode/NO10.java
@@ -1,4 +1,4 @@
-package code;
+﻿package code;
 
 public class NO10 {
 
@@ -6,7 +6,7 @@ public class NO10 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-			String str = "ab";
+		String str = "ab";
 		String pattern = ".*c";
 		System.out.println(new NO10().isMatch(str, pattern));
 	}
